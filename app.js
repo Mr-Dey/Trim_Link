3c105df5f4c825865ce2da462a4bc363ecb214c4
Mr-Dey <subhasishdeycoc@gmail.com>
2024-05-18 17:50:25 +0530

    added db

--- app.js
+++ app.js
@@ -1,6 +1,24 @@
 const express = require('express');
+const mongoose=require('mongoose');
+
 const signup=require('./route/signup');
 const login=require('./route/login');
+
+
+//database url
+const url="mongodb://localhost:27017/trimlink"
+mongoose.connect(url)
+
+//to test whether db is connected or not
+.then(()=>{
+    console.log("db Connected");
+})
+.catch((e)=>{
+    console.log("db is not connected!");
+})
+
+
+
 const app=express()
 
 //for parshing req with json paylode in req.body
@@ -8,7 +26,6 @@ app.use(express.json());
 
 //for URL-encoded payloads req.body
 app.use(express.urlencoded({extended:true}));
-
 
 //default handler
 app.use(express.static(__dirname+'/public'));
---
+++ model/usersSchema.js
@@ -0,0 +1,17 @@
+const mongoose = require('mongoose');
+
+const signupSchema=mongoose.Schema({
+    name:{
+        type:String,
+        required:true
+    },
+    email:{
+        type:String,
+        require:true,
+        unique:true
+    },
+    password:{
+        type:String,
+        required:true
+    }
+})
