9db16727b6b1fe9d1d19b26da30de5296cf96769
Mr-Dey <subhasishdeycoc@gmail.com>
2024-05-20 14:24:56 +0530

    Updated ui added smooth scroll updated typed data Fixed some bugs

--- public/js/logicDashboard.js
+++ public/js/logicDashboard.js
@@ -1,16 +1,30 @@
+//Logout
 const logout = document.querySelector('#LogOut');
-
 logout.addEventListener('click',(e)=>{
     e.preventDefault();
     window.location.href="/";
 })
 
+//for scroll
+const navToGenerate = document.querySelector('#nav_generate');
+const navToDashboard = document.querySelector('#nav_dashboard');
+const section1 = document.querySelector(".section1");
+const section2 = document.querySelector(".section2");
+scroll=(element,offset)=>{
+    window.scrollTo({
+        top:element.offsetTop-offset,
+        behavior:"smooth"
+    })
+}
+navToGenerate.addEventListener('click',()=>scroll(section1,200))
+navToDashboard.addEventListener('click',()=>scroll(section2,100))
 
-let cobyBtns=document.querySelectorAll('.copybtn')
 
 
-//This is the button inside the table row tp copy the url
-cobyBtns.forEach((btn)=>{
+
+//This is the function to copy the url of individual table
+let copyBtns=document.querySelectorAll('.copybtn')
+copyBtns.forEach((btn)=>{
     let data=btn.querySelector(".copybtn_span");
     btn.addEventListener('click',()=>{
         let url=data.textContent.trim();
